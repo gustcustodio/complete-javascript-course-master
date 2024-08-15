2ed46ff25276443bf9cf62cc757e197243b9a202
gustcustodio <gustcustodio@gmail.com>
2024-08-15 08:40:09 -0300

    att

---
+++ 01-fundamentals-part-1/assignments.js
@@ -0,0 +1,9 @@
+/*
+let country = "Brasil";
+let continent = "América do Sul";
+let population = "215";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -1,3 +1,75 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+let firstName = "Gustavo";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+// variable name conventions
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Gustavo');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); // bug
+*/
+
+/*
+let age = 30; // mutável
+age = 31;
+
+const birthYear = 1991; // imutável
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Gustavo";
+const lastName = "Felipe";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+console.log(x);
