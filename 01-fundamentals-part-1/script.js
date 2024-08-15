12f5f2546ad83b9c3ecf3e2643e571dbba0dbe57
gustcustodio <gustcustodio@gmail.com>
2024-08-15 09:28:38 -0300

    att script & challenges

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -58,6 +58,8 @@ var job = 'programmer';
 job = 'teacher';
 */
 
+/*
+// math operators
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -70,6 +72,38 @@ const firstName = "Gustavo";
 const lastName = "Felipe";
 console.log(firstName + " " + lastName);
 
+// assignment operators
 let x = 10 + 5; // 15
-x += 10; // x = x + 10
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
 console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+/* 
+// operator precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log (25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10  // right-to-left
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/ 
