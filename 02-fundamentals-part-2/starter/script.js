cd9f9ee302f0c7a8fab9b56ff030652aad15cda6
gustcustodio <gustcustodio@gmail.com>
2024-09-01 13:22:47 -0300

    strict mode and functions

---
+++ 02-fundamentals-part-2/starter/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+/** 
+ * ! strict mode lecture
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+//* x = 3.14; // this will cause an error because x is not declared
+//! keywords reserved for future JavaScript versions can NOT be used as variable names in strict mode. Examples:
+//* const interface = "Audio";
+//* const private = 534;
+*/
+/**
+ * ! functions
+function logger() {
+  console.log("My name is Gustavo");
+}
+
+//* calling / running / invoking function
+logger();
+logger();
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oragens.`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+console.log(typeof num);
+*/
+/**
+//* function declaration:
+//! hoisting effect
+const age1 = calcAge1(1991);
+function calcAge1(birthYear) {
+  // const age = 2037 - birthYear;
+  return 2037 - birthYear;
+}
+// const age1 = calcAge1(1991);
+
+//* function expression: 
+const calcAge2 = function(birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+*/
+/**
+//* arrow function: 
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`
+}
+
+console.log(yearsUntilRetirement(1991, "Gustavo"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+*/
