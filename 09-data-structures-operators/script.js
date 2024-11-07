d18863c875758cb2761fa5e4b9f8efcd3779fb3e
gustcustodio <gustcustodio@gmail.com>
2024-11-07 06:59:25 -0300

    Update script.js

--- 09-data-structures-operators/script.js
+++ 09-data-structures-operators/script.js
@@ -65,6 +65,59 @@ const rest2 = {
   owner: "Giovanni Rossi",
 };
 
+// ! property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr.slice(0, -2));
+
+// ! property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// ! entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}.`);
+}
+
+/////////////////////////////////////////////////////////////////
+/*
+! optional chaining
+
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+console.log(restaurant.openingHours.mon.open);
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+! example
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${open}.`);
+}
+
+! methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+! arrays
+// const users = [{ name: "Jonas", email: "hello@jonas.io" }];
+const users = [];
+
+console.log(users[0]?.name ?? "User array empty");
+*/
 /////////////////////////////////////////////////////////////////
 /*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
