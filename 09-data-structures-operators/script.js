62d9a2ca5a165f8a04769a769f42586e26e4836f
gustcustodio <gustcustodio@gmail.com>
2024-11-16 10:26:14 -0300

    Update script.js

--- 09-data-structures-operators/script.js
+++ 09-data-structures-operators/script.js
@@ -63,7 +63,7 @@ const rest2 = {
 
 /////////////////////////////////////////////////////////////////
 // ! String Methods Practice
-
+/*
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
@@ -86,7 +86,7 @@ for (const flight of flights.split("+")) {
 
   console.log(output);
 }
-
+*/
 /////////////////////////////////////////////////////////////////
 // ! Coding Challenge #4
 
