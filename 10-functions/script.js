fc09374d0682e9b5558989818e5b525d012e1887
gustcustodio <gustcustodio@gmail.com>
2024-12-23 12:02:55 -0300

    callback and functions returning functions

--- 10-functions/script.js
+++ 10-functions/script.js
@@ -64,3 +64,57 @@ chekIn(flight, gustavo);
 
 console.log(gustavo);
 */
+/////////////////////////////////////////////////////////////////////////////
+/*
+! callback functions !
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// high-order function //
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time //
+const high5 = function () {
+  console.log("👋");
+};
+
+document.body.addEventListener("click", high5);
+
+["Jonas", "Martha", "Adam"].forEach(high5);
+*/
+/////////////////////////////////////////////////////////////////////////////
+/*
+! functions returning functions !
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting}, ${name}!`);
+  };
+};
+
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Steven");
+
+greet("Hello")("Jonas");
+
+// challenge //
+const arrowGreet = (greeting) => (name) => {
+  console.log(`${greeting}, ${name}!`);
+};
+
+arrowGreet("Hi")("Gustavo");
+*/ 
