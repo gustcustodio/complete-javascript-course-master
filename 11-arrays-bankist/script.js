f7c6a01bfb7f560f44b8f7c8ce0139410945c5e3
gustcustodio <gustcustodio@gmail.com>
2024-12-30 06:51:00 -0300

    simple array methods

---
+++ 11-arrays-bankist/script.js
@@ -0,0 +1,154 @@
+"use strict";
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
+
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
+
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
+
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////////////////////////////////
+
+let arr = ["a", "b", "c", "d", "e"];
+
+// ! SLICE ! //
+// the slice() method returns selected elements in an array, as a new array
+// the slice() method selects from a given start, up to a (not inclusive) given end
+// the slice() method does not change the original array
+console.log("---------------------- SLICE ----------------------");
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+/*
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 3);
+console.log(citrus);
+const myBest = fruits.slice(-3, -1);
+console.log(myBest);
+*/
+// ! SPLICE ! //
+// the splice() method adds and/or removes array elements
+// the splice() method overwrites the original array
+console.log("---------------------- SPLICE ----------------------");
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
+fruits.splice(2, 2);
+console.log(fruits);
+fruits.splice(2, 1, "Lemon", "Kiwi");
+console.log(fruits);
+*/
+// ! REVERSE ! //
+// the reverse() method reverses the order of the elements in an array
+// the reverse() method overwrites the original array
+console.log("---------------------- REVERSE ----------------------");
+const arr2 = ["j", "i", "h", "g", "f"];
+arr2.reverse();
+console.log(arr2);
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.reverse();
+console.log(fruits);
+*/
+// ! CONCAT ! //
+// the concat() method concatenates (joins) two or more arrays
+// the concat() method returns a new array, containing the joined arrays
+// the concat() method does not change the existing arrays
+console.log("---------------------- CONCAT ----------------------");
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+// ! JOIN ! //
+// the join() method returns an array as a string
+// the join() method does not change the original array
+console.log("---------------------- JOIN ----------------------");
+console.log(letters.join(" - "));
+// ! AT ! //
+// the at() method returns an indexed element from an array
+// the at() method returns the same as []
+console.log("---------------------- AT ----------------------");
+const arr3 = [23, 11, 64];
+console.log(arr3[0]);
+console.log(arr3.at(0));
+// getting last array element
+console.log(arr3[arr3.length - 1]);
+console.log(arr3.slice(-1)[0]);
+console.log(arr3.at(-1));
+
+console.log("jonas".at(0));
+console.log("jonas".at(-1));
