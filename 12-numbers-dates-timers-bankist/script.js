ba6d5c70b0e59243a4d2e84c912a544be7adbb98
gustcustodio <gustcustodio@gmail.com>
2025-02-11 06:58:34 -0300

    bigInts and dates

--- 12-numbers-dates-timers-bankist/script.js
+++ 12-numbers-dates-timers-bankist/script.js
@@ -336,7 +336,6 @@ console.log(+(2.345).toFixed(2));
 */
 /*
 ! REMAINDER OPERATOR !
-
 console.log(5 % 2);
 console.log(5 / 2);
 
@@ -376,3 +375,70 @@ const transferFee2 = 1_500;
 console.log(Number("230_000"));
 console.log(parseInt("230_000"));
 */
+/*
+! BIGINT !
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(45687546879546875126548n);
+console.log(BigInt("45687546879546875126548"));
+
+! OPERATIONS !
+console.log(1000n + 1000n);
+console.log(45687546879546875126548n * 10000n);
+// console.log(Math.sqrt(16n));
+
+const huge = 45623687954236871269n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+! EXCEPTIONS !
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(20n == 20);
+console.log(20n == "20");
+console.log(typeof 20n);
+
+console.log(huge + " is REALLY big!!");
+
+! DIVISIONS !
+console.log(10n / 3n);
+console.log(10 / 3);
+*/
+// ! CREATING DATES ! //
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// ! WORKING WITH DATES ! //
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
