b9cc9da9e921767735de287f386d4e12a1735a7c
gustcustodio <gustcustodio@gmail.com>
2025-02-17 07:05:58 -0300

    create review.js

---
+++ 12-numbers-dates-timers-bankist/review.js
@@ -0,0 +1,12 @@
+"use strict";
+const date = new Date();
+// const date = Date();
+console.log(date);
+// retorna o dia do mês //
+console.log(date.getDate());
+// retorna o dia da semana //
+console.log(date.getDay());
+// retorna o ano da data na forma de quatro dígitos completa //
+console.log(date.getFullYear());
+// retorna o campo de horas //
+console.log(date.getHours());
