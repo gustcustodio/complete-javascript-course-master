047cc5a23f16049eda1ff91bd91a08ca9a165cb7
gustcustodio <gustcustodio@gmail.com>
2025-02-18 06:53:31 -0300

    review of methods

--- 12-numbers-dates-timers-bankist/review.js
+++ 12-numbers-dates-timers-bankist/review.js
@@ -2,11 +2,79 @@
 const date = new Date();
 // const date = Date();
 console.log(date);
-// retorna o dia do mês //
-console.log(date.getDate());
-// retorna o dia da semana //
-console.log(date.getDay());
-// retorna o ano da data na forma de quatro dígitos completa //
-console.log(date.getFullYear());
-// retorna o campo de horas //
-console.log(date.getHours());
+// * retorna o dia do mês * //
+// console.log("getDate");
+// console.log(date.getDate());
+// console.log(date.getUTCDate());
+// * retorna o dia da semana * //
+// console.log("getDay");
+// console.log(date.getDay());
+// console.log(date.getUTCDay());
+// * retorna o ano da data na forma de quatro dígitos completa * //
+// console.log("getFullYear");
+// console.log(date.getFullYear());
+// console.log(date.getUTCFullYear());
+// * retorna o campo de horas * //
+// console.log("getHours");
+// console.log(date.getHours());
+// console.log(date.getUTCHours());
+// * retorna o campo de milissegundos * //
+// console.log("getMilliseconds");
+// console.log(date.getMilliseconds());
+// console.log(date.getUTCMilliseconds());
+// * retorna o campo de minutos * //
+// console.log("getMinutes");
+// console.log(date.getMinutes());
+// console.log(date.getUTCMinutes());
+// * retorna o campo de mês * //
+// console.log("getMonth");
+// console.log(date.getMonth());
+// console.log(date.getUTCMonth());
+// * retorna o campo de segundos * //
+// console.log("getSeconds");
+// console.log(date.getSeconds());
+// console.log(date.getUTCSeconds());
+// * retorna representação de milissegundos * //
+// console.log("getTime");
+// console.log(date.getTime());
+// * retorna a diferença, em minutos, entre as representações local e UTC dessa data * //
+// console.log("getTimezoneOffSet");
+// console.log(date.getTimezoneOffset());
+// * configura o campo de dia do mês * //
+// date.setDate(20);
+// * configura o campo de ano (e opcionalmente mês e dia) * //
+// date.setFullYear(2026, 0, 30);
+// * configura o campo de hora (e opcionalmente os campos de minutos, segundos e milissegundos) * //
+// date.setHours(8);
+// * configura o campo de milissegundos * //
+// date.setMilliseconds(1000);
+// * configura o campo de minutos (e opcionalmente os campos de segundos e milissegundos) * //
+// date.setMinutes(50);
+// * configura o campo do mês (e opcionalmente o dia do mês) * //
+// date.setMonth(0);
+// * configura o campo de segundos (e opcionalmente o campo de milissegundos) * //
+// date.setSeconds(30);
+// * retorna uma string representando a parte da data, expressa no fuso horário local * //
+// console.log(date.toDateString());
+// * converte um objeto Date em uma string, usando o formato de data/hora ISO-8601 combinado e UTC * //
+// console.log(date.toISOString());
+// * JSON serializa um objeto date, usando toISOSString() * //
+// console.log(date.toJSON());
+// * retorna uma string representando a parte da data, expressa no fuso horário local, usando as convenções locais de formatação de data * //
+// console.log(date.toLocaleDateString());
+// * converte um objeto Date em uma string, usando o fuso horário local e as convenções de formatação locais de data * //
+// console.log(date.toLocaleString());
+// * retorna uma string representando a parte da hora da data, expressa no fuso horário local, usando as convenções de formatação locais de hora * //
+// console.log(date.toLocaleTimeString());
+// * converte um objeto Date em uma string usando o fuso horário local * //
+// console.log(date.toString());
+// * retorna uma string representando a parte da hora da data, expressa no fuso horário local * //
+// console.log(date.toTimeString());
+// * converte um objeto Date em um string, usando hora universal * //
+// console.log(date.toUTCString());
+// * converte um objeto Date e mseu formato interno de milissegundos * //
+// console.log(date.valueOf());
+// ! MÉTODOS ESTÁTICOS ! //
+// console.log(Date.now());
+// console.log(Date.parse());
+// console.log(Date.UTC());
